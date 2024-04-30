4441d528351ce949a52abb10df53ab5e32c80c64
Raj kumar R <78129713+rajkumarrptpm@users.noreply.github.com>
2024-04-30 20:46:03 +0530

    progress-bar-function-created-and-its-worked-successfully

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let playBtn = document.getElementById('play-btn');
 let video = document.querySelector('.video');
+let progressBar = document.querySelector('.progress-bar')
 
 let isVideoPlaying = false;
 
@@ -21,13 +22,27 @@ function playOrPauseVideo() {
 
 }
 
-function playorpause(event){
+function playOrPauseSpace(event){
     if(event.keyCode === 32){
         playOrPauseVideo();
     }
 }
 
+
+function updateProgressbar(event){
+    console.log(event.target.currentTime,event.target.duration);
+    progressBar.style.cssText =`
+    width: ${((event.target.currentTime/event.target.duration)*100)}%;
+
+    `;
+
+}
+
+
 // add event Listners
 playBtn.addEventListener('click',playOrPauseVideo);
 video.addEventListener('click',playOrPauseVideo);
-document.addEventListener('keydown',playorpause);
+document.addEventListener('keydown',playOrPauseSpace);
+
+// progress bar
+video.addEventListener('timeupdate',updateProgressbar)
